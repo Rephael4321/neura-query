a1e503c7b96018ac61cc7d52c3c63388ba7acca9
Rephael4321 <Rephael4321@gmail.com>
2025-04-02 20:36:41 +0300

    Updated releases page.

--- src/app/releases/page.jsx
+++ src/app/releases/page.jsx
@@ -5,6 +5,10 @@ export default function Releases() {
     <div className="mx-[20%]">
       <h1 className="text-[2rem] my-[24px]">Releases</h1>
       <Release
+        title="1.0.2 - 30/03/2025"
+        description="Client now receives and updates GUI with DB commands and their results automatically."
+      ></Release>
+      <Release
         title="1.0.1 - 30/03/2025"
         description="Metadata and provider information moved from cookies on client side to api server. This move makes fetching this information much faster by making it accessible from the api server itself. It also reduces the payload size sent in every request. And most importantly, cookies has limitations of 4096 Bytes size, meaning metadata cannot be stored if it is to big, moving to api server solves this problem."
       ></Release>
